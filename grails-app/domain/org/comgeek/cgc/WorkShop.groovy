5cbab6a59596f4e7209c22d68a90218e2c4f7dc1
qwerty_wiz <qwerty.wiz@gmail.com>
2012-07-07 17:50:28 +0900

    [#32401935] ドメインを追加しました．

---
+++ grails-app/domain/org/comgeek/cgc/WorkShop.groovy
@@ -0,0 +1,16 @@
+package org.comgeek.cgc
+
+class WorkShop {
+	
+	String title
+	Date start
+	Date end
+	String place
+	String detail
+	String owner
+	String mail
+	String password
+	
+    static constraints = {
+    }
+}
