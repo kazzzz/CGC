a098d9e7414cfeb6b9722716b771771e64daa8d9
qwerty_wiz <qwerty.wiz@gmail.com>
2012-07-10 22:32:43 +0900

    [#32401935 #32401963 #32402035 #32402161] 各画面のラベルを編集しました．また，ドメインファイルにコメントを挿入しました．基本的に各所で使う用語はドメインファイルのものを参考にしたいです．

--- grails-app/domain/org/comgeek/cgc/WorkShop.groovy
+++ grails-app/domain/org/comgeek/cgc/WorkShop.groovy
@@ -2,13 +2,21 @@ package org.comgeek.cgc
 
 class WorkShop {
 	
+	/** イベント名 */
 	String title
+	/** 開始 */
 	Date start
+	/** 終了 */
 	Date end
+	/** 開催場所 */
 	String place
+	/** 詳細 */
 	String detail
+	/** 代表者 */
 	String owner
+	/** 連絡先 */
 	String mail
+	/** 編集用パスワード */
 	String password
 	
     static constraints = {
