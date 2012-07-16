aa95766ddbeecb10211b4ce0acfcd148e31423c0
kazzzz <kazzz.bobo@gmail.com>
2012-07-16 17:51:47 +0900

    [#32401935] 最初のドメインテストコード登録

--- test/unit/org/comgeek/cgc/WorkShopTests.groovy
+++ test/unit/org/comgeek/cgc/WorkShopTests.groovy
@@ -10,8 +10,25 @@ import org.junit.*
  */
 @TestFor(WorkShop)
 class WorkShopTests {
-
-    void testSomething() {
-       fail "Implement me"
-    }
+	
+	@Before
+	void setUp() {
+		mockForConstraintsTests(WorkShop)
+	}
+	
+	@Test
+	void 通常登録() {
+		def target = new WorkShop(title:'幕張勉強会', start:new Date(), end:new Date(), place:'幕張ビル21F',
+				detail:"aaa", owner:'陣内一喜', mail:'kaz.jinnai@nttcom.co.jp', password:'aaaa')
+		assert target.validate()
+	}
+	
+	@Test
+	void blank制約() {
+		def target = new WorkShop(title:'', start:'', end:'', place:'', detail:'', owner:'', mail:'', passowrd:'')
+		assert !target.validate()
+		assert target.errors['title'] == 'blank'
+		println target.errors['end']
+		assert target.errors['start'] == 'nullable'
+	}
 }
